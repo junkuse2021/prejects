380b796b9b8e9ba443140ca06bbdb0de62d8ccbb
junkuse2021 <junkuse2021@gmail.com>
2023-06-18 17:32:15 +0530

    barely works

--- ratingvis-tmp/main.py
+++ ratingvis-tmp/main.py
@@ -1,5 +1,15 @@
 import requests
+import tkinter as tk
 import json
+
+
+
+def get_color(rating_val):
+    red = int((10-rating_val) * 25.5)
+    green = int(rating_val * 25.5)
+    blue = 0
+    return f"#{red:02x}{green:02x}{blue:02x}"
+
 
 default = 71712
 series_id = input("Enter the series id [default: good doctor]: ") or default
@@ -20,6 +30,7 @@ with open('show_details.json', 'r') as f:
     show_details = json.load(f)
 
 show = {}
+all_seasons = []
 
 no_of_seasons = show_details['number_of_seasons']
 show['number_of_seasons'] = no_of_seasons
@@ -46,13 +57,26 @@ for i in range(no_of_seasons):
     for episode in range(len(episodes)):
         rating = episodes[episode]['vote_average']
         episode_rating[f'E{episode+1}'] = rating
-
+    
     which_season = {}
     which_season[f'S{i+1}'] = episode_rating
-    show.update(which_season)
+    all_seasons.append(which_season)
 
-
+show["seasons"] = all_seasons
 with open('show_rating.json', 'w') as f:
     json.dump(show, f, indent=1)
 
 print("check show_rating.json")
+tot_seasons = len(all_seasons)
+
+
+root = tk.Tk()
+for i in range(tot_seasons):
+    current_season = all_seasons[i][f'S{i+1}'].values()
+    episode_ratings = list(current_season)
+    for eRating in range( len(episode_ratings)):
+        color = get_color(episode_ratings[eRating])
+        label = tk.Label(root, text=f"{' '*5}S{i+1} E{eRating+1}: {episode_ratings[eRating]}{' '*5}", bg=color, padx=10)
+        # label.pack()
+        label.grid(row=eRating, column=i)
+root.mainloop()
